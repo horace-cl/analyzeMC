fe741864775119d9c7b24c35b464d25fc2ea71ae
Horacio <horacio.crotte@gmail.com>
2020-09-02 15:00:05 -0500

    correcting dir names

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Analyze/MCanalyzer/',1)[0])+'/cfipython/slc6_amd64_gcc700/Analyze/MCanalyzer')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/analyzeMC/MCanalyzer/',1)[0])+'/cfipython/slc6_amd64_gcc700/analyzeMC/MCanalyzer')
