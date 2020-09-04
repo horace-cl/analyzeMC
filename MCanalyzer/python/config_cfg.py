85aacbeb04feb177220af2b9727e1856b3d5707a
Horacio Crotte <horacio.crotte@gmail.com>
2020-09-04 10:00:44 +0200

    more events

--- MCanalyzer/python/config_cfg.py
+++ MCanalyzer/python/config_cfg.py
@@ -9,14 +9,14 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-            'file:/afs/cern.ch/user/h/hcrottel/private/ONLYGEN/GS10K.root'
+            'file:/afs/cern.ch/user/h/hcrottel/private/ONLYGEN/GS.root'
             #'file:/afs/cern.ch/cms/Tutorials/TWIKI_DATA/TTJets_8TeV_53X.root'
                 )
                             )
 
 process.TFileService = cms.Service("TFileService",
 #        fileName = cms.string('Rootuple_BstoJpsiphi_2018_MiniAOD.root'),
-         fileName = cms.string('Rootuple_ONLYGEN10K.root'),                                  
+         fileName = cms.string('Rootuple_ONLYGEN25K.root'),                                  
 )
 
 process.demo = cms.EDAnalyzer('MCanalyzer'
