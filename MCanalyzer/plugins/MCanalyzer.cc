092984ce28e93bcdae4e135c80acb10d128a9724
Horacio <horacio.crotte@gmail.com>
2020-09-06 15:30:00 -0500

    new add

--- MCanalyzer/plugins/MCanalyzer.cc
+++ MCanalyzer/plugins/MCanalyzer.cc
@@ -93,8 +93,8 @@ class MCanalyzer : public edm::EDAnalyzer {
       //edm::EDGetTokenT<reco::GenParticleCollection> genCands_;
       edm::EDGetTokenT<std::vector<reco::GenParticle>> genCands_;
       
-      TLorentzVector gen_b_p4,gen_phi_p4,gen_kaon_p4,gen_muon1_p4,gen_muon2_p4, gen_gamma1_p4, gen_gamma2_p4;
-      TLorentzVector gen_b_p4J,gen_phi_p4J,gen_kaon_p4J,gen_muon1_p4J,gen_muon2_p4J, gen_gamma1_p4J, gen_gamma2_p4J;
+      //TLorentzVector gen_b_p4,gen_phi_p4,gen_kaon_p4,gen_muon1_p4,gen_muon2_p4, gen_gamma1_p4, gen_gamma2_p4;
+      //TLorentzVector gen_b_p4J,gen_phi_p4J,gen_kaon_p4J,gen_muon1_p4J,gen_muon2_p4J, gen_gamma1_p4J, gen_gamma2_p4J;
       TLorentzVector gen_b_p4CM,gen_phi_p4CM,gen_kaon_p4CM,gen_muon1_p4CM,gen_muon2_p4CM, gen_gamma1_p4CM, gen_gamma2_p4CM;
       TLorentzVector gen_b_p4CMJ,gen_phi_p4CMJ,gen_kaon_p4CMJ,gen_muon1_p4CMJ,gen_muon2_p4CMJ, gen_gamma1_p4CMJ, gen_gamma2_p4CMJ;
       TVector3       gen_b_vtx;
@@ -118,7 +118,8 @@ class MCanalyzer : public edm::EDAnalyzer {
 // constructors and destructor
 //
 MCanalyzer::MCanalyzer(const edm::ParameterSet& iConfig)
- :number_daughters(0), number_daughtersJ(0), bplus(0), costhetaL(0.0), costhetaKL(0.0), costhetaLJ(0.0), costhetaKLJ(0.0)
+ :number_daughters(0), number_daughtersJ(0), bplus(0), costhetaL(-2.0), costhetaKL(-2.0), costhetaLJ(-2.0), costhetaKLJ(-2.0)
+ //:number_daughters(0), number_daughtersJ(0), bplus(0), costhetaL(0.0), costhetaKL(0.0), costhetaLJ(0.0), costhetaKLJ(0.0)
 {
   std::cout << "INITIALIZER?" << std::endl;
   genCands_ = consumes<std::vector<reco::GenParticle>>(edm::InputTag("genParticles"));
@@ -149,13 +150,13 @@ MCanalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   bool JHOVANNYS=false;
   bool debug = false;
 
-  gen_b_p4.SetPxPyPzE(0.,0.,0.,0.);
-  gen_kaon_p4.SetPxPyPzE(0.,0.,0.,0.);
-  gen_muon1_p4.SetPxPyPzE(0.,0.,0.,0.);
-  gen_muon2_p4.SetPxPyPzE(0.,0.,0.,0.);
-  gen_b_vtx.SetXYZ(0.,0.,0.);
-  gen_gamma1_p4.SetPxPyPzE(0.,0.,0.,0.);
-  gen_gamma2_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_b_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_kaon_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_muon1_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_muon2_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_b_vtx.SetXYZ(0.,0.,0.);
+  // gen_gamma1_p4.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_gamma2_p4.SetPxPyPzE(0.,0.,0.,0.);
 
   gen_b_p4J.SetPxPyPzE(0.,0.,0.,0.);
   gen_kaon_p4J.SetPxPyPzE(0.,0.,0.,0.);
@@ -164,12 +165,12 @@ MCanalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   gen_gamma1_p4J.SetPxPyPzE(0.,0.,0.,0.);
   gen_gamma2_p4J.SetPxPyPzE(0.,0.,0.,0.);
 
-  gen_b_p4CM.SetPxPyPzE(0.,0.,0.,0.);
-  gen_kaon_p4CM.SetPxPyPzE(0.,0.,0.,0.);
-  gen_muon1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
-  gen_muon2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
-  gen_gamma1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
-  gen_gamma2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_b_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_kaon_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_muon1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_muon2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_gamma1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  // gen_gamma2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
 
   gen_b_p4CMJ.SetPxPyPzE(0.,0.,0.,0.);
   gen_kaon_p4CMJ.SetPxPyPzE(0.,0.,0.,0.);
@@ -297,134 +298,141 @@ MCanalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   }
  
 
-
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
   // from 
   // Calibration/HcalCalibAlgos/plugins/SimAnalyzerMinbias.cc
-  if (!genEvtHandle.isValid()) 
-  {
-      if (debug) std::cout << " ------------->  no HepMCProduct found" << std::endl;    
-  } 
-  HepMC::GenEvent * myGenEvent = new  HepMC::GenEvent(*(genEvtHandle->GetEvent()));
-  if (debug) std::cout << "Event with : \n"; 
-  std::cout << myGenEvent->particles_size() << " particles \n";
-  if (debug) std::cout << myGenEvent->vertices_size() << " vertices\n";
+  // if (!genEvtHandle.isValid()) 
+  // {
+  //     if (debug) std::cout << " ------------->  no HepMCProduct found" << std::endl;    
+  // } 
+  // HepMC::GenEvent * myGenEvent = new  HepMC::GenEvent(*(genEvtHandle->GetEvent()));
+  // if (debug) std::cout << "Event with : \n"; 
+  // std::cout << myGenEvent->particles_size() << " particles \n";
+  // if (debug) std::cout << myGenEvent->vertices_size() << " vertices\n";
 
 
 
-  // from
-  // Alignment/OfflineValidation/plugins/ValidationMisalignedTracker.cc:
-  // Iterate over all particles
-  for ( HepMC::GenEvent::particle_iterator p = myGenEvent->particles_begin(); p != myGenEvent->particles_end(); ++p ) { 
-    if (JHOVANNYS) break;
-    if (abs((*p)->pdg_id())!=521){
-      //if (debug) std::cout << "\t\tNot B+\n";
-      continue;
-    }
+  // // from
+  // // Alignment/OfflineValidation/plugins/ValidationMisalignedTracker.cc:
+  // // Iterate over all particles
+  // for ( HepMC::GenEvent::particle_iterator p = myGenEvent->particles_begin(); p != myGenEvent->particles_end(); ++p ) { 
+  //   if (JHOVANNYS) break;
+  //   if (abs((*p)->pdg_id())!=521){
+  //     //if (debug) std::cout << "\t\tNot B+\n";
+  //     continue;
+  //   }
 
-    if (debug) std::cout << "\tPDG ID : " << (*p)->pdg_id() << std::endl;
-    if (debug) std::cout << "\tSTATUS : " << (*p)->status() << std::endl;
+  //   if (debug) std::cout << "\tPDG ID : " << (*p)->pdg_id() << std::endl;
+  //   if (debug) std::cout << "\tSTATUS : " << (*p)->status() << std::endl;
 
-    if (debug) std::cout << "PX " << (*p)->momentum().px() << std::endl;
-    if (debug) std::cout << "PY " << (*p)->momentum().py() << std::endl;
-    if (debug) std::cout << "PZ " << (*p)->momentum().pz() << std::endl;
-    if (debug) std::cout << "ENERGY " << (*p)->momentum().e() << std::endl;
-    if (debug) std::cout << "MASS " << (*p)->momentum().m() << std::endl; 
-    std::vector<int> ids;
+  //   if (debug) std::cout << "PX " << (*p)->momentum().px() << std::endl;
+  //   if (debug) std::cout << "PY " << (*p)->momentum().py() << std::endl;
+  //   if (debug) std::cout << "PZ " << (*p)->momentum().pz() << std::endl;
+  //   if (debug) std::cout << "ENERGY " << (*p)->momentum().e() << std::endl;
+  //   if (debug) std::cout << "MASS " << (*p)->momentum().m() << std::endl; 
+  //   std::vector<int> ids;
 
-    gen_b_p4.SetPxPyPzE((*p)->momentum().px(),(*p)->momentum().py(),(*p)->momentum().pz(),(*p)->momentum().e());
+  //   gen_b_p4.SetPxPyPzE((*p)->momentum().px(),(*p)->momentum().py(),(*p)->momentum().pz(),(*p)->momentum().e());
 
-    //if (debug) std::cout << "\tDaugthers : " << (*p)->numberOfDaughters() << std::endl;
-    //std::cout << "\tDaugthers : " << std::endl;
-    int photons=0;
+  //   //if (debug) std::cout << "\tDaugthers : " << (*p)->numberOfDaughters() << std::endl;
+  //   //std::cout << "\tDaugthers : " << std::endl;
+  //   int photons=0;
 
     
-    //Ierate over its daughters
-    for(
-      HepMC::GenVertex::particle_iterator aDaughter=(*p)->end_vertex()->particles_begin(HepMC::descendants); 
-      aDaughter !=(*p)->end_vertex()->particles_end(HepMC::descendants);
-      aDaughter++)
-    {
-      //Just a vector to have control over all daugther particles
-      ids.push_back((*aDaughter)->pdg_id());
-      if (debug) std::cout << "\t\tPDG ID : " << (*aDaughter)->pdg_id() << std::endl;
-      if (debug) std::cout << "\t\tSTATUS : " << (*aDaughter)->status() << std::endl;
+  //   //Ierate over its daughters
+  //   for(
+  //     HepMC::GenVertex::particle_iterator aDaughter=(*p)->end_vertex()->particles_begin(HepMC::descendants); 
+  //     aDaughter !=(*p)->end_vertex()->particles_end(HepMC::descendants);
+  //     aDaughter++)
+  //   {
+  //     //Just a vector to have control over all daugther particles
+  //     ids.push_back((*aDaughter)->pdg_id());
+  //     if (debug) std::cout << "\t\tPDG ID : " << (*aDaughter)->pdg_id() << std::endl;
+  //     if (debug) std::cout << "\t\tSTATUS : " << (*aDaughter)->status() << std::endl;
 
-      //KAON MUST HAVE THE SAME SIGN AS THE B CHARGED MESON
-      if (abs((*aDaughter)->pdg_id())==321){
-          gen_kaon_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());}
-      // muons - INDEX 1 WILL BE ASSIGNED TO THE MUON WITH OPPOSITE SIGN WRT KAON
-      // THETA L IS THE AGNLE BEWTEEN THESE TWO (KAON - MUON1)
-	    else if (abs((*aDaughter)->pdg_id())==13){
-        //CHECK IS THE CURRENT MUON HAS THE SAME SIGN AS THE B MESON
-        if ((*p)->pdg_id()*(*aDaughter)->pdg_id() > 0){
-          gen_muon2_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
-        }
-        else {
-          gen_muon1_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
-        }
-      }
-      // IN CASE THERE ARE ANY PHOTONS
-      else if ((*aDaughter)->pdg_id()==22){
-        if (photons==0){
-          gen_gamma1_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
-        	photons=1;
-        }
-        else{
-          gen_gamma2_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
-        }
-      //if (debug) std::cout << "\t\tGrandDaughters : " << (*aDaughter)->numberOfDaughters() << std::endl;
-      }
-    }
+  //     //KAON MUST HAVE THE SAME SIGN AS THE B CHARGED MESON
+  //     if (abs((*aDaughter)->pdg_id())==321){
+  //         gen_kaon_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());}
+  //     // muons - INDEX 1 WILL BE ASSIGNED TO THE MUON WITH OPPOSITE SIGN WRT KAON
+  //     // THETA L IS THE AGNLE BEWTEEN THESE TWO (KAON - MUON1)
+	 //    else if (abs((*aDaughter)->pdg_id())==13){
+  //       //CHECK IS THE CURRENT MUON HAS THE SAME SIGN AS THE B MESON
+  //       if ((*p)->pdg_id()*(*aDaughter)->pdg_id() > 0){
+  //         gen_muon2_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
+  //       }
+  //       else {
+  //         gen_muon1_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
+  //       }
+  //     }
+  //     // IN CASE THERE ARE ANY PHOTONS
+  //     else if ((*aDaughter)->pdg_id()==22){
+  //       if (photons==0){
+  //         gen_gamma1_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
+  //       	photons=1;
+  //       }
+  //       else{
+  //         gen_gamma2_p4.SetPxPyPzE((*aDaughter)->momentum().px(),(*aDaughter)->momentum().py(),(*aDaughter)->momentum().pz(),(*aDaughter)->momentum().e());
+  //       }
+  //     //if (debug) std::cout << "\t\tGrandDaughters : " << (*aDaughter)->numberOfDaughters() << std::endl;
+  //     }
+  //   }
 
-    // NOW CREATE THE BOOST TO DILEPTON CM FRAME
-    math::XYZTLorentzVector muon1(gen_muon1_p4.Px(), gen_muon1_p4.Py(), gen_muon1_p4.Pz(), gen_muon1_p4.E());
-    math::XYZTLorentzVector muon2(gen_muon2_p4.Px(), gen_muon2_p4.Py(), gen_muon2_p4.Pz(), gen_muon2_p4.E());
-    math::XYZTLorentzVector kaon(gen_kaon_p4.Px(), gen_kaon_p4.Py(), gen_kaon_p4.Pz(), gen_kaon_p4.E());
-    math::XYZTLorentzVector bmeson(gen_b_p4.Px(), gen_b_p4.Py(), gen_b_p4.Pz(), gen_b_p4.E());
-    math::XYZTLorentzVector gamma1(gen_gamma1_p4.Px(), gen_gamma1_p4.Py(), gen_gamma1_p4.Pz(), gen_gamma1_p4.E());
-    math::XYZTLorentzVector gamma2(gen_gamma2_p4.Px(), gen_gamma2_p4.Py(), gen_gamma2_p4.Pz(), gen_gamma2_p4.E()); 
+  //   // NOW CREATE THE BOOST TO DILEPTON CM FRAME
+  //   math::XYZTLorentzVector muon1(gen_muon1_p4.Px(), gen_muon1_p4.Py(), gen_muon1_p4.Pz(), gen_muon1_p4.E());
+  //   math::XYZTLorentzVector muon2(gen_muon2_p4.Px(), gen_muon2_p4.Py(), gen_muon2_p4.Pz(), gen_muon2_p4.E());
+  //   math::XYZTLorentzVector kaon(gen_kaon_p4.Px(), gen_kaon_p4.Py(), gen_kaon_p4.Pz(), gen_kaon_p4.E());
+  //   math::XYZTLorentzVector bmeson(gen_b_p4.Px(), gen_b_p4.Py(), gen_b_p4.Pz(), gen_b_p4.E());
+  //   math::XYZTLorentzVector gamma1(gen_gamma1_p4.Px(), gen_gamma1_p4.Py(), gen_gamma1_p4.Pz(), gen_gamma1_p4.E());
+  //   math::XYZTLorentzVector gamma2(gen_gamma2_p4.Px(), gen_gamma2_p4.Py(), gen_gamma2_p4.Pz(), gen_gamma2_p4.E()); 
     
 
-    math::XYZTLorentzVector dilep = muon1+muon2;
-    ROOT::Math::Boost dileptonCMBoost(dilep.BoostToCM());
+  //   math::XYZTLorentzVector dilep = muon1+muon2;
+  //   ROOT::Math::Boost dileptonCMBoost(dilep.BoostToCM());
 
 
 
-    math::XYZTLorentzVector kaonCM(  dileptonCMBoost( kaon )  );
-    math::XYZTLorentzVector muonCM1(  dileptonCMBoost( muon1 )  );
-    math::XYZTLorentzVector muonCM2(  dileptonCMBoost( muon2 )  );
-    math::XYZTLorentzVector bmesonCM(  dileptonCMBoost( bmeson )  );
-    math::XYZTLorentzVector gamma1CM(  dileptonCMBoost( gamma1 )  );
-    math::XYZTLorentzVector gamma2CM(  dileptonCMBoost( gamma2 )  );
+  //   math::XYZTLorentzVector kaonCM(  dileptonCMBoost( kaon )  );
+  //   math::XYZTLorentzVector muonCM1(  dileptonCMBoost( muon1 )  );
+  //   math::XYZTLorentzVector muonCM2(  dileptonCMBoost( muon2 )  );
+  //   math::XYZTLorentzVector bmesonCM(  dileptonCMBoost( bmeson )  );
+  //   math::XYZTLorentzVector gamma1CM(  dileptonCMBoost( gamma1 )  );
+  //   math::XYZTLorentzVector gamma2CM(  dileptonCMBoost( gamma2 )  );
 
 
-    gen_b_p4CM.SetPxPyPzE(bmesonCM.x(), bmesonCM.y(), bmesonCM.z(), bmesonCM.t() ) ;
-    gen_kaon_p4CM.SetPxPyPzE(kaonCM.x(), kaonCM.y(), kaonCM.z(), kaonCM.t() ) ;
-    gen_muon1_p4CM.SetPxPyPzE(muonCM1.x(), muonCM1.y(), muonCM1.z(), muonCM1.t() ) ;
-    gen_muon2_p4CM.SetPxPyPzE(muonCM2.x(), muonCM2.y(), muonCM2.z(), muonCM2.t() ) ;
-    gen_gamma1_p4CM.SetPxPyPzE(gamma1CM.x(), gamma1CM.y(), gamma1CM.z(), gamma1CM.t() ) ;
-    gen_gamma2_p4CM.SetPxPyPzE(gamma2CM.x(), gamma2CM.y(), gamma2CM.z(), gamma2CM.t() ) ;
+  //   gen_b_p4CM.SetPxPyPzE(bmesonCM.x(), bmesonCM.y(), bmesonCM.z(), bmesonCM.t() ) ;
+  //   gen_kaon_p4CM.SetPxPyPzE(kaonCM.x(), kaonCM.y(), kaonCM.z(), kaonCM.t() ) ;
+  //   gen_muon1_p4CM.SetPxPyPzE(muonCM1.x(), muonCM1.y(), muonCM1.z(), muonCM1.t() ) ;
+  //   gen_muon2_p4CM.SetPxPyPzE(muonCM2.x(), muonCM2.y(), muonCM2.z(), muonCM2.t() ) ;
+  //   gen_gamma1_p4CM.SetPxPyPzE(gamma1CM.x(), gamma1CM.y(), gamma1CM.z(), gamma1CM.t() ) ;
+  //   gen_gamma2_p4CM.SetPxPyPzE(gamma2CM.x(), gamma2CM.y(), gamma2CM.z(), gamma2CM.t() ) ;
 
 
-    costhetaL = ( muonCM1.x()*muonCM2.x() 
-                       + muonCM1.y()*muonCM2.y() 
-                       + muonCM1.z()*muonCM2.z() ) / (muonCM1.P()*muonCM2.P() );
+  //   costhetaL = ( muonCM1.x()*muonCM2.x() 
+  //                      + muonCM1.y()*muonCM2.y() 
+  //                      + muonCM1.z()*muonCM2.z() ) / (muonCM1.P()*muonCM2.P() );
 
-    costhetaKL = ( muonCM1.x()*kaonCM.x()
-                       + muonCM1.y()*kaonCM.y()
-                       + muonCM1.z()*kaonCM.z() ) / (muonCM1.P()*kaonCM.P() );
+  //   costhetaKL = ( muonCM1.x()*kaonCM.x()
+  //                      + muonCM1.y()*kaonCM.y()
+  //                      + muonCM1.z()*kaonCM.z() ) / (muonCM1.P()*kaonCM.P() );
 
   
-    if (debug) std::cout << "Number of Daugthers : "<< ids.size() <<std::endl;
-    daughter_id.push_back(ids);
-    number_daughters= ids.size();
+  //   if (debug) std::cout << "Number of Daugthers : "<< ids.size() <<std::endl;
+  //   daughter_id.push_back(ids);
+  //   number_daughters= ids.size();
 
-    tree_->Fill();
+  //   tree_->Fill();
 
-    daughter_id.clear();
-    daughter_idJ.clear();
+  //   daughter_id.clear();
+  //   daughter_idJ.clear();
 
-  }
+  // }
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
+  // HORACIOS HORACIOS HORACIOS
 
 
 }
@@ -439,13 +447,13 @@ MCanalyzer::beginJob()
   edm::Service<TFileService> fs;
   tree_ = fs->make<TTree>("ntuple","B+->K+ mu mu ntuple");
 
-  tree_->Branch("gen_b_p4",     "TLorentzVector",  &gen_b_p4);
-  tree_->Branch("gen_kaon_p4",  "TLorentzVector",  &gen_kaon_p4);
-  tree_->Branch("gen_muon1_p4",  "TLorentzVector",  &gen_muon1_p4);
-  tree_->Branch("gen_muon2_p4",  "TLorentzVector",  &gen_muon2_p4);
-  tree_->Branch("gen_b_vtx",    "TVector3",        &gen_b_vtx);
-  tree_->Branch("gen_gamma1_p4",  "TLorentzVector",  &gen_gamma1_p4);
-  tree_->Branch("gen_gamma2_p4",  "TLorentzVector",  &gen_gamma2_p4);
+  // tree_->Branch("gen_b_p4",     "TLorentzVector",  &gen_b_p4);
+  // tree_->Branch("gen_kaon_p4",  "TLorentzVector",  &gen_kaon_p4);
+  // tree_->Branch("gen_muon1_p4",  "TLorentzVector",  &gen_muon1_p4);
+  // tree_->Branch("gen_muon2_p4",  "TLorentzVector",  &gen_muon2_p4);
+  // tree_->Branch("gen_b_vtx",    "TVector3",        &gen_b_vtx);
+  // tree_->Branch("gen_gamma1_p4",  "TLorentzVector",  &gen_gamma1_p4);
+  // tree_->Branch("gen_gamma2_p4",  "TLorentzVector",  &gen_gamma2_p4);
 
   tree_->Branch("gen_b_p4J",     "TLorentzVector",  &gen_b_p4J);
   tree_->Branch("gen_kaon_p4J",  "TLorentzVector",  &gen_kaon_p4J);
@@ -461,19 +469,19 @@ MCanalyzer::beginJob()
   tree_->Branch("gen_gamma1_p4CM",  "TLorentzVector",  &gen_gamma1_p4CM);
   tree_->Branch("gen_gamma2_p4CM",  "TLorentzVector",  &gen_gamma2_p4CM);
 
-  tree_->Branch("gen_b_p4CMJ",     "TLorentzVector",  &gen_b_p4CMJ);
-  tree_->Branch("gen_kaon_p4CMJ",  "TLorentzVector",  &gen_kaon_p4CMJ);
-  tree_->Branch("gen_muon1_p4CMJ",  "TLorentzVector",  &gen_muon1_p4CMJ);
-  tree_->Branch("gen_muon2_p4CMJ",  "TLorentzVector",  &gen_muon2_p4CMJ);
-  tree_->Branch("gen_gamma1_p4CMJ",  "TLorentzVector",  &gen_gamma2_p4CMJ);
-  tree_->Branch("gen_gamma2_p4CMJ",  "TLorentzVector",  &gen_gamma2_p4CMJ);
+  // tree_->Branch("gen_b_p4CMJ",     "TLorentzVector",  &gen_b_p4CMJ);
+  // tree_->Branch("gen_kaon_p4CMJ",  "TLorentzVector",  &gen_kaon_p4CMJ);
+  // tree_->Branch("gen_muon1_p4CMJ",  "TLorentzVector",  &gen_muon1_p4CMJ);
+  // tree_->Branch("gen_muon2_p4CMJ",  "TLorentzVector",  &gen_muon2_p4CMJ);
+  // tree_->Branch("gen_gamma1_p4CMJ",  "TLorentzVector",  &gen_gamma2_p4CMJ);
+  // tree_->Branch("gen_gamma2_p4CMJ",  "TLorentzVector",  &gen_gamma2_p4CMJ);
   
-  tree_->Branch("daughter_id",   "vector", &daughter_id);
+  // tree_->Branch("daughter_id",   "vector", &daughter_id);
   tree_->Branch("daughter_idJ",   "vector", &daughter_idJ);
 
-  tree_->Branch("number_daughters",  &number_daughters);
-  tree_->Branch("costhetaL",  &costhetaL);
-  tree_->Branch("costhetaKL",  &costhetaKL);
+  // tree_->Branch("number_daughters",  &number_daughters);
+  // tree_->Branch("costhetaL",  &costhetaL);
+  // tree_->Branch("costhetaKL",  &costhetaKL);
 
   tree_->Branch("number_daughtersJ",  &number_daughtersJ);
   tree_->Branch("costhetaLJ",  &costhetaLJ);
