739b524f845fe572a798f660b24090db51a0de48
Horacio Crotte <horacio.crotte@gmail.com>
2020-09-16 01:08:05 +0200

    Glob

--- MCanalyzer/python/config_cfg.py
+++ MCanalyzer/python/config_cfg.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+from glob import glob
 
 process = cms.Process("Demo")
 
@@ -6,18 +7,24 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+
+files_ = glob('/eos/user/h/hcrottel/PrivateMC-2020-b_kmumu_PHSPS/crab_PrivateMC-2020-b_kmumu_PHSPS-2020-09-14-06-57/200914_045749/0000/step0*')
+
+print(files_)
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-            #'file:/afs/cern.ch/work/h/hcrottel/private/GSnewFilter.root'
-	    'file:/afs/cern.ch/user/h/hcrottel/private/ONLYGEN/GSnewFilterOutput.root'
+		['file:'+file_ for file_ in files_]
+            #"file:/eos/user/h/hcrottel/PrivateMC-2020-b_kmumu_PHSPS/crab_PrivateMC-2020-b_kmumu_PHSPS-2020-09-10-15-12/200910_131235/0000/step0-GS-b_kmumu_PHSPS_65.root"
+	    #'file:/afs/cern.ch/work/h/hcrottel/private/GSnewFilter.root'
+	    #'file:/afs/cern.ch/user/h/hcrottel/private/ONLYGEN/GSnewFilterOutput.root'
             #'file:/afs/cern.ch/cms/Tutorials/TWIKI_DATA/TTJets_8TeV_53X.root'
                 )
                             )
 
 process.TFileService = cms.Service("TFileService",
 #        fileName = cms.string('Rootuple_BstoJpsiphi_2018_MiniAOD.root'),
-         fileName = cms.string('Rootuple_ONLYGENnewFilterOutput.root'),                                  
+         fileName = cms.string('GEN-SIM.root'),                                  
 )
 
 process.demo = cms.EDAnalyzer('MCanalyzer'
