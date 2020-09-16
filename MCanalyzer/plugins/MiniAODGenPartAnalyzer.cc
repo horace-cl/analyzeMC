05552889218d731fad63334f59c2cc956cdeebef
Horacio <horacio.crotte@gmail.com>
2020-09-16 12:50:56 -0500

    save variables

--- MCanalyzer/plugins/MiniAODGenPartAnalyzer.cc
+++ MCanalyzer/plugins/MiniAODGenPartAnalyzer.cc
@@ -15,30 +15,38 @@
  #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
  #include "DataFormats/Candidate/interface/Candidate.h"
  #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+ #include "TLorentzVector.h"
+ #include "TTree.h"
  //
  // class declaration
  //
 
 class MiniAODGenPartAnalyzer : public edm::EDAnalyzer {
-public:
-explicit MiniAODGenPartAnalyzer(const edm::ParameterSet&);
-~MiniAODGenPartAnalyzer();
-bool isAncestor(const reco::Candidate * ancestor, const reco::Candidate * particle, int &calls);
+  public:
+    explicit MiniAODGenPartAnalyzer(const edm::ParameterSet&);
+    ~MiniAODGenPartAnalyzer();
+    bool isAncestor(const reco::Candidate * ancestor, const reco::Candidate * particle, int &calls);
 
 
 
-private:
-virtual void beginJob() override;
-virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-virtual void endJob() override;
+  private:
+    virtual void beginJob() override;
+    virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+    virtual void endJob() override;
+    TLorentzVector gen_b_p4, gen_phi_p4, gen_kaon_p4, gen_muon1_p4, gen_muon2_p4, gen_gamma1_p4, gen_gamma2_p4;
+    TLorentzVector gen_b_p4CM, gen_phi_p4CM, gen_kaon_p4CM, gen_muon1_p4CM, gen_muon2_p4CM, gen_gamma1_p4CM, gen_gamma2_p4CM;
+    float costhetaL, costhetaKL
+    TTree*         tree_;
 
-edm::EDGetTokenT<edm::View<reco::GenParticle> > prunedGenToken_;
-edm::EDGetTokenT<edm::View<pat::PackedGenParticle> > packedGenToken_;
+    edm::EDGetTokenT<edm::View<reco::GenParticle> > prunedGenToken_;
+    edm::EDGetTokenT<edm::View<pat::PackedGenParticle> > packedGenToken_;
 };
 
 MiniAODGenPartAnalyzer::MiniAODGenPartAnalyzer(const edm::ParameterSet& iConfig):
 prunedGenToken_(consumes<edm::View<reco::GenParticle> >(iConfig.getParameter<edm::InputTag>("pruned"))),
-packedGenToken_(consumes<edm::View<pat::PackedGenParticle> >(iConfig.getParameter<edm::InputTag>("packed")))
+packedGenToken_(consumes<edm::View<pat::PackedGenParticle> >(iConfig.getParameter<edm::InputTag>("packed"))),
+costhetaL(-2.0), costhetaKL(-2.0)
 {
 }
 
@@ -71,6 +79,24 @@ MiniAODGenPartAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup&
   using namespace reco;
   using namespace pat;
 
+  bool debug = true;
+  bool iskaon, isMuon1, isMuon2;
+
+
+  gen_b_p4.SetPxPyPzE(0.,0.,0.,0.);
+  gen_kaon_p4.SetPxPyPzE(0.,0.,0.,0.);
+  gen_muon1_p4.SetPxPyPzE(0.,0.,0.,0.);
+  gen_muon2_p4.SetPxPyPzE(0.,0.,0.,0.);
+  gen_gamma1_p4.SetPxPyPzE(0.,0.,0.,0.);
+  gen_gamma2_p4.SetPxPyPzE(0.,0.,0.,0.);
+
+  gen_b_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  gen_kaon_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  gen_muon1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  gen_muon2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  gen_gamma1_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+  gen_gamma2_p4CM.SetPxPyPzE(0.,0.,0.,0.);
+
   // Pruned particles are the one containing "important" stuff
   Handle<edm::View<reco::GenParticle> > pruned;
   iEvent.getByToken(prunedGenToken_,pruned);
@@ -83,25 +109,84 @@ MiniAODGenPartAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup&
   //let's try to find all status1 originating directly from a B meson decay 
 
   for(size_t i=0; i<pruned->size();i++){
+    iskaon=false;
+    isMuon2=false;
+    isMuon1=false;
     if(abs((*pruned)[i].pdgId()) == 521){
     //if(abs((*pruned)[i].pdgId()) > 500 && abs((*pruned)[i].pdgId()) <600){
       const Candidate * bMeson = &(*pruned)[i];
-      std::cout << "PdgID: " << bMeson->pdgId() << " pt " << bMeson->pt() << " eta: " << bMeson->eta() << " phi: " << bMeson->phi() << std::endl;
-      std::cout << "  found daugthers: " << std::endl;
+      gen_b_p4.SetPtEtaPhiM(bMeson->pt(),bMeson->eta(),bMeson->phi(),bMeson->mass());
+
+      if (debug){
+        std::cout << "PdgID: " << bMeson->pdgId() << " pt " << bMeson->pt() << " eta: " << bMeson->eta() << " phi: " << bMeson->phi() << std::endl;
+        std::cout << "  found daugthers: " << std::endl;
+      }
+      
       for(size_t j=0; j<packed->size();j++){
         int Ncalls=0;
         //get the pointer to the first survied ancestor of a given packed GenParticle in the prunedCollection 
-        const Candidate * motherInPrunedCollection = (*packed)[j].mother(0) ;
+        const Candidate * motherInPrunedCollection = (*packed)[j].mother(0);
+        //Lets check if the daughter particles are K+(321) and muons(13)
         if(motherInPrunedCollection != nullptr && isAncestor( bMeson , motherInPrunedCollection, Ncalls)){
+          if (abs((*packed)[j].pdgId()) == 321){
+            iskaon=true;
+            gen_kaon_p4.SetPtEtaPhiM((*packed)[j].pt(),(*packed)[j].eta(),(*packed)[j].phi(),(*packed)[j].mass());
+          }
+          else if(abs((*packed)[j].pdgId()) == 13){
+            if((*packed)[j].pdgId()*bMeson->pdgId()<0){
+              isMuon1=true;
+              gen_muon1_p4.SetPtEtaPhiM((*packed)[j].pt(),(*packed)[j].eta(),(*packed)[j].phi(),(*packed)[j].mass());        
+            }
+            else {
+              isMuon2=true;
+              gen_muon2_p4.SetPtEtaPhiM((*packed)[j].pt(),(*packed)[j].eta(),(*packed)[j].phi(),(*packed)[j].mass()); 
+            }
+          }
           std::cout << "     PdgID: " << (*packed)[j].pdgId() << " pt " << (*packed)[j].pt() << " eta: " << (*packed)[j].eta() << " phi: " << (*packed)[j].phi() << std::endl;
           std::cout << "           calls: " << Ncalls << std::endl;
         }
       }
+
+      if (iskaon && isMuon1 && isMuon2){
+        math::XYZTLorentzVector muon1J(gen_muon1_p4.Px(), gen_muon1_p4.Py(), gen_muon1_p4.Pz(), gen_muon1_p4.E());
+        math::XYZTLorentzVector muon2(gen_muon2_p4.Px(), gen_muon2_p4.Py(), gen_muon2_p4.Pz(), gen_muon2_p4.E());
+        math::XYZTLorentzVector kaon(gen_kaon_p4.Px(), gen_kaon_p4.Py(), gen_kaon_p4.Pz(), gen_kaon_p4.E());
+        math::XYZTLorentzVector bmeson(gen_b_p4.Px(), gen_b_p4.Py(), gen_b_p4.Pz(), gen_b_p4.E());
+        math::XYZTLorentzVector gamma1(gen_gamma1_p4.Px(), gen_gamma1_p4.Py(), gen_gamma1_p4.Pz(), gen_gamma1_p4.E());
+        math::XYZTLorentzVector gamma2(gen_gamma2_p4.Px(), gen_gamma2_p4.Py(), gen_gamma2_p4.Pz(), gen_gamma2_p4.E());
+
+
+        math::XYZTLorentzVector dilep = muon1+muon2;
+        ROOT::Math::Boost dileptonCMBoost(dilep.BoostToCM());
+
+        math::XYZTLorentzVector kaonCM(  dileptonCMBoost( kaon )  );
+        math::XYZTLorentzVector muonCM1(  dileptonCMBoost( muon1 )  );
+        math::XYZTLorentzVector muonCM2(  dileptonCMBoost( muon2 )  );
+        math::XYZTLorentzVector bmesonCM(  dileptonCMBoost( bmeson )  );
+        math::XYZTLorentzVector gamma1CM(  dileptonCMBoost( gamma1 )  );
+        math::XYZTLorentzVector gamma2CM(  dileptonCMBoost( gamma2 )  );
+
+        gen_b_p4CM.SetPxPyPzE(bmesonCM.x(), bmesonCM.y(), bmesonCM.z(), bmesonCM.t() ) ;
+        gen_kaon_p4CM.SetPxPyPzE(kaonCM.x(), kaonCM.y(), kaonCM.z(), kaonCM.t() ) ;
+        gen_muon1_p4CM.SetPxPyPzE(muonCM1.x(), muonCM1.y(), muonCM1.z(), muonCM1.t() ) ;
+        gen_muon2_p4CM.SetPxPyPzE(muonCM2.x(), muonCM2.y(), muonCM2.z(), muonCM2.t() ) ;
+        gen_gamma1_p4CM.SetPxPyPzE(gamma1CM.x(), gamma1CM.y(), gamma1CM.z(), gamma1CM.t() ) ;
+        gen_gamma2_p4CM.SetPxPyPzE(gamma2CM.x(), gamma2CM.y(), gamma2CM.z(), gamma2CM.t() ) ;
+
+
+        costhetaL = ( muonCM1.x()*muonCM2.x() 
+                           + muonCM1.y()*muonCM2.y() 
+                           + muonCM1.z()*muonCM2.z() ) / (muonCM1.P()*muonCM2.P() );
+
+        costhetaKL = ( muonCM1.x()*kaonCM.x()
+                           + muonCM1.y()*kaonCM.y()
+                           + muonCM1.z()*kaonCM.z() ) / (muonCM1.P()*kaonCM.P() );
+      }
     }
 
   }
-
-
+ tree_->Fill();
+ 
 }
 
 
@@ -109,6 +194,34 @@ MiniAODGenPartAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup&
 void 
 MiniAODGenPartAnalyzer::beginJob()
 {
+  std::cout << "Beginning analyzer job" << std::endl;
+
+  edm::Service<TFileService> fs;
+
+  tree_ = fs->make<TTree>("ntuple","B+->K+ mu mu ntuple");
+
+  tree_->Branch("gen_b_p4",     "TLorentzVector",  &gen_b_p4);
+  tree_->Branch("gen_kaon_p4",  "TLorentzVector",  &gen_kaon_p4);
+  tree_->Branch("gen_muon1_p4",  "TLorentzVector",  &gen_muon1_p4);
+  tree_->Branch("gen_muon2_p4",  "TLorentzVector",  &gen_muon2_p4);
+  tree_->Branch("gen_gamma1_p4",  "TLorentzVector",  &gen_gamma1_p4);
+  tree_->Branch("gen_gamma2_p4",  "TLorentzVector",  &gen_gamma2_p4);
+
+  tree_->Branch("gen_b_p4CM",     "TLorentzVector",  &gen_b_p4CM);
+  tree_->Branch("gen_kaon_p4CM",  "TLorentzVector",  &gen_kaon_p4CM);
+  tree_->Branch("gen_muon1_p4CM",  "TLorentzVector",  &gen_muon1_p4CM);
+  tree_->Branch("gen_muon2_p4CM",  "TLorentzVector",  &gen_muon2_p4CM);
+  tree_->Branch("gen_gamma1_p4CM",  "TLorentzVector",  &gen_gamma2_p4CM);
+  tree_->Branch("gen_gamma2_p4CM",  "TLorentzVector",  &gen_gamma2_p4CM);
+  
+
+  //tree_->Branch("daughter_id",   "vector", &daughter_id);
+
+  //tree_->Branch("number_daughters",  &number_daughters);
+  tree_->Branch("costhetaL",  &costhetaL);
+  tree_->Branch("costhetaKL",  &costhetaKL);
+
+  //tree_->Branch("Nbplus", &bplus);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
