0116379e568224ef21b21c90bf308c716386c18c
Horacio <horacio.crotte@gmail.com>
2020-09-16 13:04:43 -0500

    Outputfile

--- MCanalyzer/python/mc_config_cfg.py
+++ MCanalyzer/python/mc_config_cfg.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from glob import glob
+import sys
 
 process = cms.Process("Demo")
 
@@ -7,19 +8,33 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+
+#Source
 files = glob("/eos/user/h/hcrottel/PrivateMC-2020-b_kmumu_PHSPS/crab_PrivateMC-2020-b_kmumu_PHSPS-2020-09-14-06-57/200914_045749/0000/step3*")
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-	['file:'+file_ for file_ in files[:10]]
-	#"root://cms-xrd-global.cern.ch//store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/F6EDDC10-8DFC-E311-BC5D-0025905A60D6.root"
-	
+		['file:'+file_ for file_ in files[:10]]	
 	)
 )
 
-process.demo = cms.EDAnalyzer('MiniAODGenPartAnalyzer',
-packed = cms.InputTag("packedGenParticles"),
-pruned = cms.InputTag("prunedGenParticles")
+
+#Analyzer
+process.MiniSim = cms.EDAnalyzer('MiniAODGenPartAnalyzer',
+	packed = cms.InputTag("packedGenParticles"),
+	pruned = cms.InputTag("prunedGenParticles")
 )
 
 
-process.p = cms.Path(process.demo)
+
+#OUTPUT
+if len(sys.argv)>1:
+	file_name = sys.argv[1]+'.root'
+else:
+	file_name = 'output.root'
+process.TFileService = cms.Service("TFileService",
+        fileName = cms.string(file_name),                                  
+)
+
+
+
+process.p = cms.Path(process.MiniSim)
